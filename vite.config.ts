0a79bced1b9197ca0698df1c138bb4bdc0d380db
Andy Chuo <zeonism@gmail.com>
2025-09-23 20:25:28 +0800

    Update vite.config.ts
    Add VitePWA import for automatic service worker updates.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    VitePWA({ registerType: 'autoUpdate' })
   ].filter(Boolean),
   resolve: {
     alias: {
